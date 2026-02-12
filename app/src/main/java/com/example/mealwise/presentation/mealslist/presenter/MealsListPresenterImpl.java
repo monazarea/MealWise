9695e1809b934a15907180b51e759dd027515541
monazarea <94869444+monazarea@users.noreply.github.com>
2026-02-12 07:11:14 +0200

    implement manual search for meals list using RxJava and MVP

--- app/src/main/java/com/example/mealwise/presentation/mealslist/presenter/MealsListPresenterImpl.java
+++ app/src/main/java/com/example/mealwise/presentation/mealslist/presenter/MealsListPresenterImpl.java
@@ -1,20 +1,26 @@
 package com.example.mealwise.presentation.mealslist.presenter;
 
+import com.example.mealwise.data.meals.models.Meal;
 import com.example.mealwise.data.meals.models.MealResponse;
 import com.example.mealwise.data.meals.repository.MealsRepository;
 import com.example.mealwise.presentation.mealslist.view.MealsListView;
 import com.example.mealwise.utils.Constants;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
-public class MealsListPresenterImpl {
+public class MealsListPresenterImpl implements MealsListPresenter{
 
     private MealsListView view;
     private MealsRepository repository;
     private CompositeDisposable disposables = new CompositeDisposable();
+    private List<Meal> allMeals = new ArrayList<>();
 
     public MealsListPresenterImpl(MealsListView view, MealsRepository repository) {
         this.view = view;
@@ -46,6 +52,7 @@ public class MealsListPresenterImpl {
                             .subscribe(
                                     response -> {
                                         view.hideLoading();
+                                        this.allMeals = response.getMeals();
                                         view.showMeals(response.getMeals());
                                     },
                                     throwable -> {
@@ -55,6 +62,28 @@ public class MealsListPresenterImpl {
                             )
             );
         }
+    }
+
+    @Override
+    public void filterMeals(String query) {
+        if (allMeals == null || allMeals.isEmpty()) return;
+
+        if (query.isEmpty()) {
+            view.showMeals(allMeals);
+            return;
+        }
+
+        disposables.add(
+                Observable.fromIterable(allMeals)
+                        .filter(meal -> meal.getName().toLowerCase().contains(query.toLowerCase()))
+                        .toList()
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                filteredList -> view.showMeals(filteredList),
+                                throwable -> view.showError(throwable.getMessage())
+                        )
+        );
     }
 
     public void onDestroy() {
